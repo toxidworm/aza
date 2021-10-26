e2dc8c96c0a84aaa84fc0d1dae2fd02d2bfa5997
ToxidWorm <79449659+toxidworm@users.noreply.github.com>
2021-10-26 23:45:17 +0300

    Added hex method

--- aza.py
+++ aza.py
@@ -1,9 +1,13 @@
 import argparse
 import base64
 import os
+import binascii
 from colorama import Fore, Back, Style, init
 if os.name == 'nt':
 	init()
+	os.system("cls")
+else:
+	os.system("clear")
 print(f"""{Fore.CYAN}
     _              ___                       _           
    /_\\   _____ _  | __|_ _  __ _ _ _  _ _ __| |_ ___ _ _ 
@@ -14,7 +18,7 @@ print(f"""{Fore.CYAN}
 print
 parser = argparse.ArgumentParser(description='Aza encryptor arguments')
 parser.add_argument("--text", type=str, help="Text to encrypt")
-parser.add_argument("--method", type=str, help="Method to encrypt (base64, binary)")
+parser.add_argument("--method", type=str, help="Method to encrypt (base64, binary, hex)")
 parser.add_argument("--tofile", type=str, help="Create file with results, 1 - Create and encrypt, 0 - Encrypt without creating")
 args = parser.parse_args()
 
@@ -38,6 +42,14 @@ if args.method == 'binary':
 		ft = open("out.txt","w+")
 		f = open( 'out.txt', 'w' )
 		f.write(f"This text encrypted by AZA Cryptor\nResult of encoding:\n{str(res)}")
+		f.close()
+if args.method == 'hex':
+	s = args.text.encode("utf-8").hex()
+	print(f'{Fore.YELLOW}Result: {s}{Style.RESET_ALL}')
+	if args.tofile == '1':
+		ft = open("out.txt","w+")
+		f = open( 'out.txt', 'w' )
+		f.write(f"This text encrypted by AZA Cryptor\nResult of encoding:\n{s}")
 		f.close()
 #else:
 #	print('Unknown method')
